f5f574cf6e79ae8db081224569b535cc86ebfa5d
NiiCue <185397314@qq.com>
2025-12-07 21:17:09 +0800

    feature-power: 添加幂运算功能

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
-// main.cpp - 简单计算器程序 v1.2
+// main.cpp - 简单计算器程序 v2.0 (feature-power分支)
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int add(int a, int b) {
@@ -14,11 +15,25 @@ int multiply(int a, int b) {
     return a * b;
 }
 
+double divide(int a, int b) {
+    if (b == 0) {
+        cout << "错误：除数不能为0！" << endl;
+        return 0;
+    }
+    return (double)a / b;
+}
+
+double power(int base, int exp) {
+    return pow(base, exp);
+}
+
 int main() {
     int x = 10, y = 20;
-    cout << "===== 简单计算器 =====" << endl;
+    cout << "===== 简单计算器 v2.0 =====" << endl;
     cout << x << " + " << y << " = " << add(x, y) << endl;
     cout << x << " - " << y << " = " << subtract(x, y) << endl;
     cout << x << " * " << y << " = " << multiply(x, y) << endl;
+    cout << x << " / " << y << " = " << divide(x, y) << endl;
+    cout << x << " ^ 2 = " << power(x, 2) << endl;
     return 0;
 }
