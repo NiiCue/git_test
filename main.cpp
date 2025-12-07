968017b097b155d3c6e3bd0efb27c70c245b0873
NiiCue <185397314@qq.com>
2025-12-07 21:04:47 +0800

    第四次提交：添加除法功能（含除零检查）

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// main.cpp - 简单计算器程序 v1.2
+// main.cpp - 简单计算器程序 v1.3
 #include <iostream>
 using namespace std;
 
@@ -14,11 +14,20 @@ int multiply(int a, int b) {
     return a * b;
 }
 
+double divide(int a, int b) {
+    if (b == 0) {
+        cout << "错误：除数不能为0！" << endl;
+        return 0;
+    }
+    return (double)a / b;
+}
+
 int main() {
     int x = 10, y = 20;
-    cout << "===== 简单计算器 =====" << endl;
+    cout << "===== 简单计算器 v1.3 =====" << endl;
     cout << x << " + " << y << " = " << add(x, y) << endl;
     cout << x << " - " << y << " = " << subtract(x, y) << endl;
     cout << x << " * " << y << " = " << multiply(x, y) << endl;
+    cout << x << " / " << y << " = " << divide(x, y) << endl;
     return 0;
 }
