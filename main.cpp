3a4af51f4fc3585e39d648ce66e03d44a9232f94
NiiCue <185397314@qq.com>
2025-12-07 21:12:45 +0800

    添加测试文件test.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// main.cpp - 简单计算器程序 v1.3
+// main.cpp - 简单计算器程序 v1.2
 #include <iostream>
 using namespace std;
 
@@ -14,20 +14,11 @@ int multiply(int a, int b) {
     return a * b;
 }
 
-double divide(int a, int b) {
-    if (b == 0) {
-        cout << "错误：除数不能为0！" << endl;
-        return 0;
-    }
-    return (double)a / b;
-}
-
 int main() {
     int x = 10, y = 20;
-    cout << "===== 简单计算器 v1.3 =====" << endl;
+    cout << "===== 简单计算器 =====" << endl;
     cout << x << " + " << y << " = " << add(x, y) << endl;
     cout << x << " - " << y << " = " << subtract(x, y) << endl;
     cout << x << " * " << y << " = " << multiply(x, y) << endl;
-    cout << x << " / " << y << " = " << divide(x, y) << endl;
     return 0;
 }
