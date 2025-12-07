5caf944c34dbf6840e07d5ac90aeccf699fb5aa9
NiiCue <185397314@qq.com>
2025-12-07 21:03:49 +0800

    第二次提交：添加减法功能

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// main.cpp - 简单计算器程序 v1.0
+// main.cpp - 简单计算器程序 v1.1
 #include <iostream>
 using namespace std;
 
@@ -6,10 +6,14 @@ int add(int a, int b) {
     return a + b;
 }
 
+int subtract(int a, int b) {
+    return a - b;
+}
+
 int main() {
     int x = 10, y = 20;
     cout << "Hello Git!" << endl;
     cout << x << " + " << y << " = " << add(x, y) << endl;
+    cout << x << " - " << y << " = " << subtract(x, y) << endl;
     return 0;
 }
-
