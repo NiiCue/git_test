5f6dc11d90fb1496bb26aebf4338849d6d33c303
NiiCue <185397314@qq.com>
2025-12-07 21:04:09 +0800

    第三次提交：添加乘法功能

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// main.cpp - 简单计算器程序 v1.1
+// main.cpp - 简单计算器程序 v1.2
 #include <iostream>
 using namespace std;
 
@@ -10,10 +10,15 @@ int subtract(int a, int b) {
     return a - b;
 }
 
+int multiply(int a, int b) {
+    return a * b;
+}
+
 int main() {
     int x = 10, y = 20;
-    cout << "Hello Git!" << endl;
+    cout << "===== 简单计算器 =====" << endl;
     cout << x << " + " << y << " = " << add(x, y) << endl;
     cout << x << " - " << y << " = " << subtract(x, y) << endl;
+    cout << x << " * " << y << " = " << multiply(x, y) << endl;
     return 0;
 }
